6a332102704ab1878d237eb0fef4b995586c163b
Asmet Sahoo <176515010+AsMetOP@users.noreply.github.com>
2026-05-08 22:22:31 +0530

    Add: majority-element - submission-0

---
+++ Data Structures & Algorithms/majority-element/submission-0.py
@@ -0,0 +1,11 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        freq_map = dict()
+        for i in range(0, len(nums)):
+            if nums[i] in freq_map:
+                freq_map[nums[i]] += 1
+            else:
+                freq_map[nums[i]] = 1
+        for key in freq_map:
+            if freq_map[key] > len(nums) // 2:
+                return key
